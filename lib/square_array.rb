9304d5c481d587a1ceb397e3ac4e09088e692495
Ross Perkel <rossperkel@gmail.com>
2019-07-04 13:41:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   i = 0
   newArr = []
   while (i < array.length) do
-    newArr.push(array[i] ** array[i])
+    newArr.push(array[i]**array[i])
     i += 1
   end
   newArr
